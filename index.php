bebe1bf0e9cf1ce72977e6865eecc4432f9c1a6c
DavidRCHS <david_tusan@hotmail.com>
2024-09-02 13:53:56 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+use Slim\Factory\AppFactory;
+
+require( __DIR__ . '/vendor/autoload.php' );
+
+$app = AppFactory::create();
+
+$app->get( '/', function( $request, $response, $args ) {
+  $response->getBody()->write( 'Hello, World!' );
+  return $response;
+});
+
+
+$app->run();
